8847dd82f6f29cb5376de16e194a99dd0fd1e3ce
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-04-23 17:56:35 +0300

    connection to the mongo db

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 import express from "express";
 import { PORT } from "./config.js";
+import mongoose from "mongoose";
+
 
 const app = express(); 
 
+
+app.get('/', (req, res) => {
+    // console.log(req);
+    
+    return res.status(234).send('sdfg');
+}) 
+
 app.listen(PORT, () => console.log(`Listening on port - ${PORT}`));
 
-
+mongoose
+.connect('mongodb://127.0.0.1:27017/bookStore')
+.then(()=> {
+    console.log('App connected to the database!');
+})
+.catch((err) => {
+    console.log(err);
+});
