b6ddd8b1bbb271e857d0510544a08933a042afc1
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-04-28 00:54:16 +0300

    route for add book

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -33,6 +33,7 @@ const HomePage = () => {
             <div className="flex justify-between items-center">
                 <h1 className="text-3xl my-8">Books List</h1>
                 <Link to='books/create'>
+                    <MdOutlineAddBox className="text-sky-800 text-4xl"/>
                 </Link>
             </div>
             {loading ? (
