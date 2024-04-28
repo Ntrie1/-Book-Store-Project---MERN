a1c1ac9a4bf2623c5caa71add2c2a16c22e5c201
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-04-28 18:42:42 +0300

    Update DetailsBook.jsx

--- frontend/src/pages/DetailsBook.jsx
+++ frontend/src/pages/DetailsBook.jsx
@@ -1,25 +1,72 @@
 import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
 
-const DetailsBook = () =>{
+const DetailsBook = () => {
     const [book, setBook] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-
-    const params = useParams();
-    const bookId = params.id;
+    const { id } = useParams();
 
     useEffect(() => {
-        
-        fetch(`http://localhost:5000/books/${bookId}`)
-        .then((res) => res.json())
-        .then((data) => setBook(data))
+        setLoading(true)
+
+        fetch(`http://localhost:5000/books/${id}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setLoading(false);
+                setBook(data);
+            })
+            .catch((error) => {
+                console.log(error.message);
+                setLoading(false);
+            })
     }, []);
 
     console.log(book);
 
 
     return (
-        <div>DetailsBook</div>
+        <div className="p-4">
+            <BackButton />
+            <h1 className="text-3xl my-4">Show Book</h1>
+            {loading ? (
+                <Spinner />
+            ) : (
+                <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
+                    <div className="my-4">
+                        <span className="text-xl mr-4 text-gray-500">Id: </span>
+                        <span>{book._id}</span>
+                    </div>
+
+                    <div className="my-4">
+                        <span className="text-xl mr-4 text-gray-500">Title: </span>
+                        <span>{book.title}</span>
+                    </div>
+
+                    <div className="my-4">
+                        <span className="text-xl mr-4 text-gray-500">Author: </span>
+                        <span>{book.author}</span>
+                    </div>
+
+                    <div className="my-4">
+                        <span className="text-xl mr-4 text-gray-500">Publish Year: </span>
+                        <span>{book.publishYear}</span>
+                    </div>
+
+                    <div className="my-4">
+                        <span className="text-xl mr-4 text-gray-500">Created at: </span>
+                        <span>{new Date(book.createdAt).toString()}</span>
+                    </div>
+
+                    <div className="my-4">
+                        <span className="text-xl mr-4 text-gray-500">Last Updated: </span>
+                        <span>{new Date(book.updatedAt).toString()}</span>
+                    </div>
+                </div>
+            )}
+        </div>
     )
 }
 
