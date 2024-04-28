96a3548c26221c8421b13ee2e785ab4069f6097f
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-04-28 18:17:07 +0300

    successfully fetched the data in edit component

--- frontend/src/pages/DetailsBook.jsx
+++ frontend/src/pages/DetailsBook.jsx
@@ -1,6 +1,23 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 
 const DetailsBook = () =>{
+    const [book, setBook] = useState([]);
+
+
+    const params = useParams();
+    const bookId = params.id;
+
+    useEffect(() => {
+        
+        fetch(`http://localhost:5000/books/${bookId}`)
+        .then((res) => res.json())
+        .then((data) => setBook(data))
+    }, []);
+
+    console.log(book);
+
+
     return (
         <div>DetailsBook</div>
     )
