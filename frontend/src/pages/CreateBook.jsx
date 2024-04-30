0614cad13bc60263973122a9f875f1c8a36f55ff
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-04-30 22:21:14 +0300

    Update CreateBook.jsx

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,25 +1,24 @@
-import React, { useState } from "react"
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import { useNavigate } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
-import BackButton from "../components/BackButton";
-import Spinner from "../components/Spinner";
 
-const CreateBook = () => {
+const CreateBooks = () => {
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
     const [publishYear, setPublishYear] = useState('');
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
     const { enqueueSnackbar } = useSnackbar();
+
     const handleSaveBook = () => {
         const data = {
             title,
             author,
-            publishYear
+            publishYear,
         };
-
         setLoading(true);
-
         fetch('http://localhost:5000/books', {
             method: 'POST',
             headers: {
@@ -27,57 +26,60 @@ const CreateBook = () => {
             },
             body: JSON.stringify(data),
         })
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error('Network response was not okay.')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
                 }
-                return res.json();
-            })
-            .then(() => {
                 setLoading(false);
-                enqueueSnackbar('Book created successfully!', { variant: 'success' });
+                enqueueSnackbar('Book created successfully', { variant: 'success' });
                 navigate('/');
             })
-            .catch((error) => {
+            .catch(error => {
                 setLoading(false);
                 enqueueSnackbar('Error', { variant: 'error' });
-            })
-    }
+                console.error('There was an error!', error);
+            });
+    };
 
     return (
-        <div className="p-4">
+        <div className='p-4'>
             <BackButton />
-            <h1 className="text-3xl my-4">Create Book</h1>
+            <h1 className='text-3xl my-4'>Create Book</h1>
             {loading ? <Spinner /> : ''}
-            <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
-                <div className="my-4">
-                    <label htmlFor="" className="text-xl mr-4 text-gray-500">Title</label>
-                    <input type="text"
+            <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+                <div className='my-4'>
+                    <label className='text-xl mr-4 text-gray-500'>Title</label>
+                    <input
+                        type='text'
                         value={title}
                         onChange={(e) => setTitle(e.target.value)}
-                        className="border-2 border-gray-500"
+                        className='border-2 border-gray-500 px-4 py-2 w-full'
                     />
-                    </div>
-                    <div className="my-4">
-                    <label htmlFor="" className="text-xl mr-4 text-gray-500">Author</label>
-                      <input type="text"
-                        value={author}
-                        onChange={(e) => setTitle(e.target.value)}
-                        className="border-2 border-gray-500"
-                    />
-                    </div>
-                    <div className="my-4">
-                    <label htmlFor="" className="text-xl mr-4 text-gray-500">Publish Year</label>
-                      <input type="text"
-                        value={publishYear}
-                        onChange={(e) => setTitle(e.target.value)}
-                        className="border-2 border-gray-500"
-                    />
-                    
                 </div>
+                <div className='my-4'>
+                    <label className='text-xl mr-4 text-gray-500'>Author</label>
+                    <input
+                        type='text'
+                        value={author}
+                        onChange={(e) => setAuthor(e.target.value)}
+                        className='border-2 border-gray-500 px-4 py-2  w-full '
+                    />
+                </div>
+                <div className='my-4'>
+                    <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+                    <input
+                        type='number'
+                        value={publishYear}
+                        onChange={(e) => setPublishYear(e.target.value)}
+                        className='border-2 border-gray-500 px-4 py-2  w-full '
+                    />
+                </div>
+                <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+                    Save
+                </button>
             </div>
         </div>
-    )
+    );
 }
 
-export default CreateBook;
+export default CreateBooks;
