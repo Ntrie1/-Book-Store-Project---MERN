144bcf1c9e6158d6442d7fa1d442fe8c51b4f238
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-04-30 22:31:20 +0300

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,9 +1,109 @@
-import React from "react"
+import React, { useState, useEffect } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
 
-const EditBook = () =>{
-    return (
-        <div>EditBook</div>
-    )
-}
+const EditBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { enqueueSnackbar } = useSnackbar();
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`http://localhost:5000/books/${id}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not okay.');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setAuthor(data.author);
+        setPublishYear(data.publishYear);
+        setTitle(data.title);
+        setLoading(false);
+      })
+      .catch(error => {
+        setLoading(false);
+        enqueueSnackbar('An error happened. Please check console', { variant: 'error' });
+        console.error(error);
+      });
+  }, [id]);
+
+  const handleEditBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    fetch(`http://localhost:5000/books/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => {
+        console.log(response);
+        if (!response.ok) {
+          throw new Error('Network response was not okay.');
+        }
+        setLoading(false);
+        enqueueSnackbar('Book edited successfully', { variant: 'success' });
+        navigate('/');
+      })
+      .catch(error => {
+        setLoading(false);
+        enqueueSnackbar('An error happened. Please check console', { variant: 'error' });
+        console.error(error);
+      });
+  };
+
+  return (
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Edit Book</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Title</label>
+          <input
+            type='text'
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Author</label>
+          <input
+            type='text'
+            value={author}
+            onChange={e => setAuthor(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+          <input
+            type='number'
+            value={publishYear}
+            onChange={e => setPublishYear(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <button className='p-2 bg-sky-300 m-8' onClick={handleEditBook}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default EditBook;
